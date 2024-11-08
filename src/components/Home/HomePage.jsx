0f2d9608474f36799879662b985752cbf01886bf
john-mc7800 <130221320+john-mc7800@users.noreply.github.com>
2024-11-08 18:05:37 +0500

    remove color

--- src/components/Home/HomePage.jsx
+++ src/components/Home/HomePage.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function HomePage() {
   return (
-    <div className='h-[1400px]  bg-red-200 w-full'>
+    <div className='h-[1400px]  bg-red-00 w-full'>
      
      <p className=' text-customGray'>home1</p> 
     </div>
